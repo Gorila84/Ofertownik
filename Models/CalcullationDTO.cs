55e5984827d72aa52b3253dfcb145c5f959e7c70
Gorila84 <maciek.sikora84@gmail.com>
2022-11-06 22:47:54 +0100

    add CalcullationDTO

---
+++ Models/CalcullationDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class CalcullationDTO
+    {
+        public string UserId { get; set; }
+        public int ProductId { get; set; }
+        public int MachineId { get; set; }
+        public int Minutes { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+     
+    }
+}
---
+++ Ofertownik/Controllers/CalcullationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ofertownik.Services.IServices;
+
+namespace Ofertownik.Controllers
+{
+    public class CalcullationController : ApiController
+    {
+        private readonly ICalcullationService _calcullationService;
+
+        public CalcullationController(ICalcullationService calcullationService)
+        {
+            _calcullationService = calcullationService;
+        }
+    }
+}
